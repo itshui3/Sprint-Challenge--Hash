290d61b4f112b1fc67d3dc04d0a2e478c37ed4e7
Hui <jeffrey.x.hui@gmail.com>
2020-05-08 08:59:51 -0700

    ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,17 @@
 def get_indices_of_item_weights(weights, length, limit):
-
+    if length < 2:
+        return None
+    cache = {}
     """
     YOUR CODE HERE
     """
-
-    return None
+    for index, item in enumerate(weights):
+        if limit - item in cache:
+            #Found the pair
+            if index > cache[limit - item]:
+                return (index, cache[limit-item])
+            else:
+                return (cache[limit-item], index)
+        else:
+            #No pair
+            cache[item] = index
